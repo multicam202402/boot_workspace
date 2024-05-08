52218f5324340249c505e466f856d7b0682c6b68
multicampus202402 <multicam202402@gmail.com>
2024-05-08 15:14:55 +0900

    금일수업

--- MovieApp/src/main/java/com/sds/movieapp/controller/RestMovieController.java
+++ MovieApp/src/main/java/com/sds/movieapp/controller/RestMovieController.java
@@ -1,25 +1,31 @@
 package com.sds.movieapp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sds.movieapp.document.Comments;
-import com.sds.movieapp.domain.Actor;
-import com.sds.movieapp.domain.Director;
-import com.sds.movieapp.domain.Genre;
+import com.sds.movieapp.domain.CommentsDoc;
 import com.sds.movieapp.domain.MemberDoc;
 import com.sds.movieapp.domain.MovieDoc;
-import com.sds.movieapp.domain.Nation;
+
+import kr.co.shineware.nlp.komoran.core.Komoran;
+import kr.co.shineware.nlp.komoran.model.KomoranResult;
+import kr.co.shineware.util.common.model.Pair;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
 public class RestMovieController {
 
+	@Autowired
+	private Komoran komoran;
+	
 	//영화평 등록 요청을 처리 
 	@PostMapping("/movie/comments")
-	public ResponseEntity regist(MemberDoc member, MovieDoc movie, Comments comments) {
+	public ResponseEntity regist(MemberDoc member, MovieDoc movie, CommentsDoc commentsDoc) {
 		
 		System.out.println("호출여부 ");
 		
@@ -50,7 +56,18 @@ public class RestMovieController {
 		}
 
 		//영화평
-		System.out.println("영화평 "+comments.getContent());
+		System.out.println("영화평 "+commentsDoc.getContent());
+		
+		//코모란 동작 테스트 
+		KomoranResult result = komoran.analyze(commentsDoc.getContent());//영화평 말뭉치를 형태소 단위로 분석해본다
+		List<Pair<String, String>> list = result.getList();
+		System.out.println(list);
+		
+		/*
+		for(Pair<String, String> pair : list) {
+			System.out.println(""+pair.getFirst()+", "+pair.getSecond());
+		}
+		*/
 		
 		return null;
 	}
