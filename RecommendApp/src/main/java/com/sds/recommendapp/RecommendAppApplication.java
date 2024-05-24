16e8ea66321acbc54a07076611dbe5c2e3592a78
multicampus202402 <multicam202402@gmail.com>
2024-05-24 09:59:33 +0900

    금일수업

---
+++ RecommendApp/src/main/java/com/sds/recommendapp/RecommendAppApplication.java
@@ -0,0 +1,13 @@
+package com.sds.recommendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendAppApplication.class, args);
+	}
+
+}
